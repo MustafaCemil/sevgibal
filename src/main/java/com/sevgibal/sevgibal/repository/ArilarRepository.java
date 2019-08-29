6069b05f41b39a7f6b051e3f73c9e7a7953709d7
MustafaCemil <mstf75cml@gmail.com>
2019-08-29 23:46:05 +0300

    First initialize

--- src/main/java/com/sevgibal/sevgibal/repository/ArilarRepository.java
+++ src/main/java/com/sevgibal/sevgibal/repository/ArilarRepository.java
@@ -3,5 +3,5 @@ package com.sevgibal.sevgibal.repository;
 import com.sevgibal.sevgibal.entity.Arilar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public abstract class ArilarRepository implements JpaRepository<Arilar,Long> {
+public interface ArilarRepository extends JpaRepository<Arilar, Long> {
 }
